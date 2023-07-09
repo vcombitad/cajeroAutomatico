dcb3e7f8cc8dd3cf6f1a66723458ed173ef6d44b
vcombitad <vcombitad@ucentral.edu.co>
2023-07-09 14:28:53 -0500

    Se hacen unos ajustes en el performance, ahora los usuarios son enviados desde js y no desde el html, con el fin de mejorar la actualización de los datos del usuario

--- main.js
+++ main.js
@@ -1,20 +1,35 @@
-const cuentas = [
-    { nombre: 'Mali', saldo: 200, password: "sharingannomali14"},
-    { nombre: 'Gera', saldo: 290, password: "summerforever2020"},
-    { nombre: 'Maui', saldo: 67, password: "venecia1980"},
-  ];
+const cuentas = [];
+
+    cuentas.push({ name: 'Mali', price: 200, password: "sharingannomali14"})
+    
+    cuentas.push({ name: 'Gera', price: 290, password: "summerforever2020"})
+    
+    cuentas.push({ name: 'Maui', price: 67, password: "venecia1980"})
+  
+
+  for (element of cuentas) {
+    const select=document.getElementById('usuario');
+    
+    const option=document.createElement('option');
+    select.appendChild(option)
+    
+    option.innerText= element.name;
+   
+}
 
   function verificacionContraseña () {
-    let contraseña= document.getElementById('passwordUsuario').value;
-    let cuentausuario= document.getElementById('usuario').value;
-    let ingresoUsuarios=document.getElementById('datosIngreso');
 
-if (contraseña!=cuentas[cuentausuario].password) {
+    const cuentausuario= document.getElementById('usuario').value;
+    const usuarioIndex = cuentas.findIndex(item=>item.name==cuentausuario);
+    const ingresoUsuarios=document.getElementById('datosIngreso');
+    const contraseña= document.getElementById('passwordUsuario').value;
+
+if (contraseña!=cuentas[usuarioIndex].password) {
 alert('incorrecto')
 }else {
     const section= document.getElementById('datosUsuario');
     const div1=document.createElement('div');
-    const divTexto=document.createTextNode(`Bienvenido/a ${cuentas[cuentausuario].nombre}`);
+    const divTexto=document.createTextNode(`Bienvenido/a ${cuentausuario}`);
     div1.appendChild(divTexto);
     section.appendChild(div1);
     div1.setAttribute("class", "bienvenido")
@@ -26,14 +41,15 @@ alert('incorrecto')
   }
 
 function consultar () {
-    let cuentausuario= document.getElementById('usuario').value; 
-    let saldo= cuentas[cuentausuario].saldo;
+    const cuentausuario= document.getElementById('usuario').value;
+    const usuarioIndex = cuentas.findIndex(item=>item.name==cuentausuario);
+    const saldo= cuentas[usuarioIndex].price;
     
 
     const parrafo= document.getElementById('saldoActual');
     const article= document.getElementById('saldoUsuario');
     saldoUsuario.removeAttribute("class", "d-none");
-    parrafo.innerText=`Saldo actual en tu cuenta es de ${saldo}`
+    parrafo.innerText=`Saldo actual en tu cuenta es de $${saldo}`
     //div2.setAttribute("class", "saldoCuenta")
     //movimientoSeleccionado.setAttribute("class", 'd-none')
     const inputRetirar=document.getElementById('retirarDinero');
@@ -44,7 +60,8 @@ function consultar () {
 
 function ingresarDinero() {
     let cuentausuario= document.getElementById('usuario').value;
-    let saldo= cuentas[cuentausuario].saldo;
+    const usuarioIndex = cuentas.findIndex(item=>item.name==cuentausuario);
+    let saldo= cuentas[usuarioIndex].price;
     const dineroIngresar=document.getElementById('adicionarDinero').value;
     const dineroIngresado=parseFloat(dineroIngresar);
     const nuevoSaldo=parseFloat( dineroIngresado + saldo);
@@ -53,9 +70,9 @@ function ingresarDinero() {
     
     if(nuevoSaldo<=990) {
         
-        cuentas[cuentausuario].saldo=nuevoSaldo;
+        cuentas[usuarioIndex].price=nuevoSaldo;
         const parrafo=document.getElementById('saldoActual');
-        parrafo.innerText=`Tu nuevo saldo es de ${nuevoSaldo}`
+        parrafo.innerText=`Tu nuevo saldo es de $${nuevoSaldo}`
         
     }else {
         alert('no puedes tener mas de $990 en tu cuenta, por favor ingresa menos dinero')
@@ -65,7 +82,8 @@ function ingresarDinero() {
 
 function res() {
     let cuentausuario= document.getElementById('usuario').value;
-    let saldo= cuentas[cuentausuario].saldo;
+    const usuarioIndex = cuentas.findIndex(item=>item.name==cuentausuario);
+    let saldo= cuentas[usuarioIndex].price;
     const dineroRetirar=document.getElementById('retirarDinero').value;
     const dineroRetirado=parseFloat(dineroRetirar);
     const nuevoSaldo=parseFloat( saldo - dineroRetirado);
@@ -74,9 +92,9 @@ function res() {
     
     if(nuevoSaldo>=10) {
         
-        cuentas[cuentausuario].saldo=nuevoSaldo;
+        cuentas[usuarioIndex].price=nuevoSaldo;
         const parrafo=document.getElementById('saldoActual');
-        parrafo.innerText=`Tu nuevo saldo es de ${nuevoSaldo}`
+        parrafo.innerText=`Tu nuevo saldo es de $${nuevoSaldo}`
 
     }else {
         alert('no puedes tener menos de $10 en tu cuenta, por favor retira menos dinero')
